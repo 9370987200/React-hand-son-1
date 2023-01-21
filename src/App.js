750943054b3399b48409ab70c8d67b483824fd16
9370987200 <shivanibhagyawant122@gmail.com>
2023-01-21 16:42:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import User from './User.js'
+import './User.css'
+
+function App() {
+
+  return (
+    <div>
+     <User/>
+    </div>
+
+    
+  );
+}
+
+export default App;
